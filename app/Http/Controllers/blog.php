dbb8baad20511aa86340ff7a41b10754b80d1bc1
Eimantas Dima <73961810+LynJuice@users.noreply.github.com>
2023-09-22 16:18:08 +0300

    Update blog.php

--- app/Http/Controllers/blog.php
+++ app/Http/Controllers/blog.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class blog extends Controller
 {
-    //
+    public function index()
+    {
+        return view('blog');
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
 }
