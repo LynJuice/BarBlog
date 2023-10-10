56735afe4ffb2e4da299adc8a7b25fed5411c1e3
Eimantas Dima <73961810+LynJuice@users.noreply.github.com>
2023-10-10 10:16:17 +0300

    Controlleriai padaryti resurciniai

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatagoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,14 +25,16 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 // blogs .index
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 
+Route::resource('catagory', CatagoryController::class);
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', [BlogController::class, 'index'])->name('blogs');
+    //Route::get('/dashboard', [BlogController::class, 'index'])->name('blogs');
     
-    //Route::get('/dashboard', function () {
-    //    return view('dashboard');
-    //})->name('dashboard');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
